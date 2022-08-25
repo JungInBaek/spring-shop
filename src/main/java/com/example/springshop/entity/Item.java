6f62768c48b9c2b9bd0875ed7177a0293fe0c7bb
JungInBaek <44565979+JungInBaek@users.noreply.github.com>
2022-08-25 16:39:45 +0900

    Item 엔티티 설계

---
+++ src/main/java/com/example/springshop/entity/Item.java
@@ -0,0 +1,40 @@
+package com.example.springshop.entity;
+
+import com.example.springshop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;                        //  상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;                  //  상품명
+
+    @Column(nullable = false)
+    private int price;                      //  가격
+
+    @Column(nullable = false)
+    private int stockNumber;                //  재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;              //  상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;  //  상품 판매 상태
+
+    private LocalDateTime regTime;          //  등록 시간
+
+    private LocalDateTime updateTime;       //  수정 시간
+}
---
+++ src/main/java/com/example/springshop/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.springshop.repository;
+
+import com.example.springshop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
